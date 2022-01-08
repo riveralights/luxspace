8c68a3947e3d18faa95a75a97a69118a5ebe37ac
alicaputriazz <alicaputriazz@gmail.com>
2022-01-08 10:35:30 +0700

    Menyelesaikan seluruh halaman Homepage

--- src/assets/pages/Home.js
+++ src/assets/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Hero from "../parts/Hero";
 import Header from "../parts/Header";
+import BrowseRoom from "assets/parts/Home/BrowseRoom";
+import JustArrived from "assets/parts/Home/JustArrived";
+import Clients from "assets/parts/Clients";
+import Sitemap from "assets/parts/Sitemap";
+import Footer from "assets/parts/Footer";
 
 export default function Home(props) {
     return (
         <>
             <Header />
             <Hero />
+            <BrowseRoom />
+            <JustArrived />
+            <Clients />
+            <Sitemap />
+            <Footer />
         </>
     );
 }
